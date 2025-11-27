42fee6327fd7dae31c2776ea6806d249f72409a8
[shiranezu] <aweleoluonye@gmail.com>
2025-11-27 11:59:31 +0100

    commented out lines

--- train.py
+++ train.py
@@ -28,10 +28,9 @@ X_train, X_test, y_train, y_test = train_test_split(
     X, y, test_size=0.2, random_state=42
 )
 
-# Train
 model.fit(X_train, y_train)
 
-
+# calculates accuracy
 y_pred = model.predict(X_test)
 mse = mean_squared_error(y_test, y_pred)
 rmse = sqrt(mse)
