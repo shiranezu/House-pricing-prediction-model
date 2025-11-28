1d967a4592b4748e66a49c0c30c4319b2905b5e2
[shiranezu] <aweleoluonye@gmail.com>
2025-11-28 11:42:01 +0100

    ...

--- app.py
+++ app.py
@@ -64,31 +64,7 @@ if st.button("Predict House Price"):
     st.success(f"Predicted Median House Value: **${prediction:,.2f}**")
 
 
-
-# st.write("### 📊 Feature Distribution (Bar Chart)")
-
-# selected_feature = st.selectbox(
-#     "Select a feature to visualize:",
-#     X.columns
-# )
-
-# # Bin continuous values into categories (bar chart works better this way)
-# binned_values = pd.cut(X[selected_feature], bins=20)
-
-# value_counts = binned_values.value_counts().sort_index()
-
-# fig, ax = plt.subplots(figsize=(10, 4))
-# ax.bar(value_counts.index.astype(str), value_counts.values)
-# ax.set_title(f"Bar Chart of {selected_feature}")
-# ax.set_xlabel("Binned Values")
-# ax.set_ylabel("Frequency")
-
-# plt.xticks(rotation=45)
-
-# st.pyplot(fig)
-
-
-st.write("Histogram Feature Distribution")
+st.write("### Feature Distribution (Histogram)")
 
 selected_feature = st.selectbox(
     "Select a feature to visualize:",
@@ -96,7 +72,7 @@ selected_feature = st.selectbox(
 )
 
 fig, ax = plt.subplots()
-ax.bar(X[selected_feature], bins=30, edgecolor='black')
+ax.hist(X[selected_feature], bins=30, edgecolor='black')
 ax.set_title(f"Distribution of {selected_feature}")
 ax.set_xlabel(selected_feature)
 ax.set_ylabel("Frequency")
